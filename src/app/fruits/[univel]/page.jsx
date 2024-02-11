fe9f4673542a9ffacf40ae86af537efae7cf915a
Ajmubby <ajuwonmubarak4@gmail.com>
2024-02-11 08:01:15 +0100

    feat : add style

--- src/app/fruits/[univel]/page.jsx
+++ src/app/fruits/[univel]/page.jsx
@@ -5,19 +5,19 @@ export default function page({params}) {
     const fruitCard = fruits.find(fruit => fruit.name.split('').join('-') == params.univel)
 
   return (
-    <div>
+    <div className='bg-zinc-200 h-[100vh] px-5'>
         
-        <h2 className=' text-center border-b-2 border-stone-800 capitalize'>
+        <h2 className=' text-center border-b-2 border-stone-800 capitalize mx-20'>
             <span className='font-bold'>Name: </span>{fruitCard.name}
         </h2>
         <h2>
-            <span className='font-bold'>Price: </span>${fruitCard.price}
+            <span className='font-bold font-serif'>Price: </span>${fruitCard.price}
         </h2>
         <h2>
-            <span className='font-bold'>Type: </span>{fruitCard.type}
+            <span className='font-bold font-serif'>Type: </span>{fruitCard.type}
         </h2>
         <h2>
-            <span className='font-bold'>Des: </span>{fruitCard.description}
+            <span className='font-serif font-bold'>Item Description: </span>{fruitCard.description}
         </h2>
     </div>
   )
